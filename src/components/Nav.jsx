1a817aae4545c34a5b1f09f07346cf4df53ff627
altuninovich@gmail.ru <root@DESKTOP-EFJ8J1R.localdomain>
2021-06-08 08:39:17 +0300

    alert

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Navbar, Nav, Button, FormControl } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { NavLink, Link } from 'react-router-dom';
 import * as actions from '../actions/index';
